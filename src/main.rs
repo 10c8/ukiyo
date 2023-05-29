d6c6bf1fa3efb1027c7980bd9aae22f6bb5bc13d
10c8 <2660859+10c8@users.noreply.github.com>
2023-05-29 17:04:41 -0300

    fix: better example

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,49 @@ mod scanner;
 
 fn main() {
     let input = r#"
-list -> each 1..=3 do
-  suffix -> case it of
-    1 => "st"
-    2 => "nd"
-    3 => "rd"
-    _ => "th"
+int -> re /[0-9]+/
 
-  thing -> gen <| temp 0.6
+name -> gen
+age -> gen <| int <| stop ","
+class -> gen
+mantra -> gen <| temp 0.7
+strength -> gen <| int <| stop ","
 
-  "${it}${suffix} thing: ${thing}."
+armor -> take valid_armor
+weapon -> take valid_weapon
+
+items -> gen <| temp 0.7 <| rep 3 >> join it ",\n"
+
+examples ->
+  each example_list
+    entities -> each it.entities " - ${it.name}: ${it.time}\n"
+
+    """
+    Sentence: ${it.sentence}
+    Entities and dates:
+    ${entities}
+    Reasoning: ${it.reasoning}
+    Anachronism: ${it.answer}
+    """
+  >> join "\n\n"
 
 """
-Here's a list of things similar to ${input}:
-${list}
+The following is a character profile for an RPG game in JSON format:
+```json
+{
+  "id": "${id}",
+  "description": "${description}",
+  "name": "${name}",
+  "age": ${age},
+  "class": "${class}",
+  "mantra": "${mantra}",
+  "strength": ${strength},
+  "armor": "${armor}",
+  "weapon": "${weapon}",
+  "items": [
+    ${items}
+  ]
+}
 """
 "#;
 
