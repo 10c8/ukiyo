3631a0f5fb1fe4f3d0c51863cf8c0f8f29fdd419
10c8 <2660859+10c8@users.noreply.github.com>
2023-05-29 23:17:00 -0300

    feature: colorful examples lol

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ mod lexer;
 mod parser;
 mod scanner;
 
+use colored::Colorize;
+
 fn main() {
     let input = r#"
 # int -> re /[0-9]+/
@@ -15,7 +17,7 @@ strength -> gen <| int <| stop ","
 armor -> take valid_armor
 weapon -> take valid_weapon
 
-items -> gen <| temp 0.7 <| rep 3 # >> join it ",\n"
+items -> gen <| temp 0.7 <| rep 3 >> join it ",\n"
 
 examples ->
   each example_list
@@ -28,7 +30,7 @@ examples ->
     Reasoning: ${it.reasoning}
     Anachronism: ${it.answer}
     """
-  # >> join "\n\n"
+  >> join it "\n\n"
 
 """
 The following is a character profile for an RPG game in JSON format:
@@ -51,13 +53,17 @@ The following is a character profile for an RPG game in JSON format:
 "#;
 
     let example = r#"
-color -> case it of
-  "apple"  => "red"
-  "banana" => "yellow"
-  "orange" => "orange"
+to_color -> case it of
+  "apples"  => "red"
+  "bananas" => "yellow"
+  "oranges" => "orange"
   _ => "unknown"
 
-apple_color -> color "apple"
+colors ->
+  each 0..=3 do
+    color -> to_color it
+    "${it} are ${color}!"
+  # >> join it "\n"
 
 "Apples are ${apple_color}!"
 "#;
@@ -70,7 +76,19 @@ apple_color -> color "apple"
     let mut lexer_copy = lexer.clone();
     loop {
         let token = lexer_copy.next();
-        println!("{:?}", token);
+        let token_str = format!("{:?}", token);
+        let token_str = match token {
+            lexer::Token::EOF => token_str.bright_black(),
+            lexer::Token::Identifier(_) => token_str.bright_blue(),
+            lexer::Token::String(_) | lexer::Token::Regex(_) => token_str.bright_green(),
+            lexer::Token::Number(_) => token_str.bright_magenta(),
+            lexer::Token::Keyword(_) => token_str.bright_red(),
+            lexer::Token::Newline | lexer::Token::Indent(_) | lexer::Token::Dedent(_) => {
+                token_str.bright_yellow()
+            }
+            _ => token_str.bright_cyan(),
+        };
+        print!("{}\n", token_str);
 
         if token == lexer::Token::EOF {
             break;
